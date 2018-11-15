e9df92c3a3562f8cd5c2f871830d047d15c7a027
caponp <pierre.capon@epitech.eu>
2018-11-15 14:29:35 +0300

    remove class

--- writer.py
+++ writer.py
@@ -3,33 +3,24 @@
 import argparse
 from stegano import lsb
 
-class writer:
+def openAndHide(self, filepath, identifier):
+    secret = lsb.hide(filepath, identifier)
+    secret.save(self.output)
 
-    def __init__(self, filePath, output):
-        self.filePath = filePath
-        self.output = output
-        print(self.filePath)
-        print(self.output)
+def openAndReveal(self, filepath):
+    clearMessage = lsb.reveal(filepath)
+    print(clearMessage)
+    return (clearMessage)
 
-    def openAndHide(self, identifier):
-        secret = lsb.hide(self.filePath, identifier)
-        secret.save(self.output)
-
-    def openAndReveal(self):
-        clearMessage = lsb.reveal(self.output)
-        print(clearMessage)
-        return (clearMessage)
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('file', metavar='f',
-                        help='Path to file to write')
-    parser.add_argument('output', metavar='o',
-                        help='outpute filename')
-    args = parser.parse_args()
-    objWrite = writer(args.file, args.output)
-    objWrite.openAndHide('TEST')
-    objWrite.openAndReveal()
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('file', metavar='f',
+#                         help='Path to file to write')
+#     parser.add_argument('output', metavar='o',
+#                         help='outpute filename')
+#     args = parser.parse_args()
+#     objWrite = writer(args.file, args.output)
+#     objWrite.openAndReveal()
+#
+# if __name__ == "__main__":
+#     main()
